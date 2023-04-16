bc516ee4f0f8703fe8d98bb87721245c62fe506d
wahyu-93 <wahyu.wahyudi1005@gmail.com>
2023-04-17 01:53:08 +0800

    init project

---
+++ app/Models/Customer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Authenticatable
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'email', 'password'];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
+
+    public function invoices()
+    {
+        return $this->hasMany(Invoice::class);
+    }
+
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+}
